6e8a2376f77619b6b18c4d31aff0c1f272dc3afd
SriHarsha <sriharsha1507@gmail.com>
2016-06-13 23:38:04 -0500

    Added three buttons || Selected data can be saved to CSV

--- api.js
+++ api.js
@@ -68,6 +68,69 @@ $(function() {
             filtershow("shrg");
         }
     });
+   
+   var g_tag=[];
+   var g_data=[];
+   var g_inputs=[];
+    $("#sall").click(function () {
+    var tags = $("tr td.tag");
+    var tag = [];
+    var data= [];
+    var inputs = [];
+
+    $(tags).each(function()
+    {
+        tag.push($(this).text().trim());
+    });
+    g_tag = tag;
+    // console.log(tag);
+
+    $("tr td.data").each(function()
+    {
+        data.push($(this).text().trim());
+    });
+    g_data = data;
+    var temp = $(".csv");
+
+    $(temp).each(function()
+    {
+        inputs.push($(this)[0]);
+    });
+    g_inputs = inputs;
+});
+    var output = [];
+    var result= [];
+
+    $("#button").click(function () {
+        var tags = $("tr td.tag");
+        var alldata = [];
+        for(i=0; i<tags.length; i++){
+            alldata.push({tag: g_tag[i], data: g_data[i], myinput: g_inputs[i]})
+        }
+                // console.log("hello tag push check"+alldata);
+                // console.log("hello here"+alldata);
+
+
+        for(i=0; i<alldata.length; i++){
+            if(alldata[i].myinput.checked) {
+                output.push(alldata[i]);
+            }
+        }
+        console.log(output);
+        for(i=0;i<output.length;i++)
+        {
+            result.push({tag:output[i].tag,data:output[i].data});
+        }
+    });
+
+    $("#final").on("click",function()
+    {
+
+        // console.log(result);
+        JSONToCSVConvertor(result,"Output",true);
+
+    });
+
 });//End of Document Ready
 
 function load_es_data(){
@@ -317,6 +380,79 @@ function filterhide(input)
                 $(this).parent().show();
             }
         });
+    }
+
+    function JSONToCSVConvertor(JSONData, ReportTitle, ShowLabel) {
+        //If JSONData is not an object then JSON.parse will parse the JSON string in an Object
+        var arrData = typeof JSONData != 'object' ? JSON.parse(JSONData) : JSONData;
+
+        var CSV = '';
+        //Set Report title in first row or line
+
+        CSV += ReportTitle + '\r\n\n';
+
+        //This condition will generate the Label/Header
+        if (ShowLabel) {
+            var row = "";
+
+            //This loop will extract the label from 1st index of on array
+            for (var index in arrData[0]) {
+
+                //Now convert each value to string and comma-seprated
+                row += index + ',';
+            }
+
+            row = row.slice(0, -1);
+
+            //append Label row with line break
+            CSV += row + '\r\n';
+        }
+
+        //1st loop is to extract each row
+        for (var i = 0; i < arrData.length; i++) {
+            var row = "";
+
+            //2nd loop will extract each column and convert it in string comma-seprated
+            for (var index in arrData[i]) {
+                row += '"' + arrData[i][index] + '",';
+            }
+
+            row.slice(0, row.length - 1);
+
+            //add a line break after each row
+            CSV += row + '\r\n';
+        }
+
+        if (CSV == '') {
+            alert("Invalid data");
+            return;
+        }
+
+        //Generate a file name
+        var fileName = "MyReport_";
+        //this will remove the blank-spaces from the title and replace it with an underscore
+        fileName += ReportTitle.replace(/ /g,"_");
+
+        //Initialize file format you want csv or xls
+        var uri = 'data:text/csv;charset=utf-8,' + escape(CSV);
+
+        // Now the little tricky part.
+        // you can use either>> window.open(uri);
+        // but this will not work in some browsers
+        // or you will not get the correct file extension
+
+        //this trick will generate a temp <a /> tag
+        var link = document.createElement("a");
+        link.href = uri;
+
+        //set the visibility hidden so it will not effect on your web-layout
+        link.style = "visibility:hidden";
+        link.download = fileName + ".csv";
+
+        //this part will append the anchor tag and remove it after automatic click
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
     }
 
 
